4d0168b124ac7658d90ff6d3e647fdc96f1060ca
howiezhao <howiezhaohr@hotmail.com>
2020-08-11 15:13:53 +0800

    refactor: update some code

--- test.py
+++
@@ -1,7 +0,0 @@
-from niutrans import Niutrans
-from client import Translator
-
-NIUTRANS_API_KEY = '1ad1a209b7ea5404974b6799b7244c43'
-niutrans = Niutrans(api_key=NIUTRANS_API_KEY)
-translator = Translator('/home/a/Desktop/niutrans', niutrans=niutrans)
-print(translator.translate('测试', to_lang='en'))
